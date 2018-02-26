b9261314f7f317008e93833390cee94a2b94ffcb
Nick Teets <nickteets@gmail.com>
2018-02-26 14:52:08 -0600

    Add constructor to Controls component

--- src/Controls.js
+++ src/Controls.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
 class Controls extends React.Component { 
+  
+  constructor() {
+    super();
+
+    this.state = {
+      inputValue: '',
+    };
+  }
+
   render() {
     return (
       <input type="text" placeholder="Search here" />
     )
   }
+
 };
 
 export default Controls;
