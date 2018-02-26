f61970f9a4f0db75df192f2ed4a3ebc706a252d1
Nick Teets <nickteets@gmail.com>
2018-02-26 13:55:35 -0600

    Add redux, redux dev tools

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import React from 'react'
+import { render } from 'react-dom'
+
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './reducers'
+
+import App from './App'
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+const store = createStore(rootReducer, devTools);
+
+render(
+  <Provider store={ store } >
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
