fc11f72fad1812188895ee8e5e29c503744ce8f5
Nick Teets <nickteets@gmail.com>
2018-02-27 10:20:37 -0600

    Conditional render albums list

--- src/ReleaseList.js
+++ src/ReleaseList.js
@@ -2,16 +2,25 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 class ReleaseList extends React.Component{
+  constructor() {
+    super();
+  }
+
   render() {
     return (
-      <h3>{this.props.releaseType.toUpperCase()}</h3>
+      <div>
+      { 
+        !!this.props.albumResults.length &&
+        <h3>{this.props.releaseType.toUpperCase()}</h3>
+      }
+      </div>
     )
   }
 }
 
-const mapStateToProps = store => {
-  
-}
+const mapStateToProps = store => ({
+  albumResults: store.albumResults, 
+})
 
 const mapDispatchToProps = dispatch => {
 
