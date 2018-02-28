124ee6bfe9297c8538e684ab41e6db0e75a70376
Nick Teets <nickteets@gmail.com>
2018-02-27 23:58:25 -0600

    Prettier test

--- src/Controls/Controls-actions.test.js
+++ src/Controls/Controls-actions.test.js
@@ -1,23 +1,21 @@
-import * as actions from './Controls-actions';
+import * as actions from "./Controls-actions";
 
-describe('Controls actions', () => {
-
-  it('should create an action to add a search input', () => {
-    const text = "Beyoncé"
+describe("Controls actions", () => {
+  it("should create an action to add a search input", () => {
+    const text = "Beyoncé";
     const expectedAction = {
-      type: 'INPUT_VALUE',
-      inputValue: "Beyoncé",
-    }
-    expect(actions.searchInputValue(text)).toEqual(expectedAction)
-  })
+      type: "INPUT_VALUE",
+      inputValue: "Beyoncé"
+    };
+    expect(actions.searchInputValue(text)).toEqual(expectedAction);
+  });
 
-  it('should create an action to add an artist id', () => {
-      const id = "1212"
-      const expectedAction = {
-        type: 'ARTIST_ID',
-        artistId: "1212",
-      }
-      expect(actions.searchSubmit(id)).toEqual(expectedAction)
-    })
-    
-})
+  it("should create an action to add an artist id", () => {
+    const id = "1212";
+    const expectedAction = {
+      type: "ARTIST_ID",
+      artistId: "1212"
+    };
+    expect(actions.searchSubmit(id)).toEqual(expectedAction);
+  });
+});
